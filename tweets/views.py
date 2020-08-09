93f0e1d4ae470cca3824f26e6dabbfe67e7fcd51
Danny Vu <danieldukevu@outlook.com>
2020-08-09 14:55:11 +0800

    url routing

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Tweet
+
+def home_view(request,*args, **kwargs):
+    return HttpResponse("<h1>Hello World</h1>")
+
+
+def tweet_detail_view(request,tweet_id, *args, **kwargs):
+    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
